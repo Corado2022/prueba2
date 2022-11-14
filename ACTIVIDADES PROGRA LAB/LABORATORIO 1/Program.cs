646709c493adbd5086de217114ce9c85a43f707f
Corado2022 <111150239+Corado2022@users.noreply.github.com>
2022-11-13 20:05:18 -0600

    Add files via upload

---
+++ ACTIVIDADES PROGRA LAB/LABORATORIO 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea01_1103421_Kenneth_Barillas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+
+            Console.WriteLine("Escriba su nombre:");
+            String Nombre = Console.ReadLine();
+            Console.WriteLine("Escriba su edad:");
+            String Edad = Console.ReadLine();
+            Console.WriteLine("Escriba su carrera:");
+            String Carrera = Console.ReadLine();
+            Console.WriteLine("Escriba su carné:");
+            String Carné = Console.ReadLine();
+
+            Console.WriteLine("Soy " + Nombre + " tengo " + Edad + " años y estudio la carrera de " + Carrera + " mi número de carné es " + Carné);
+         
+
+            Console.ReadKey();
+        }
+    }
+}
